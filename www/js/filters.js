9a09b9c8c7b61c4670c2d5f5d076531c38429397
Witek Galecki <w.galecki@clearcode.cc>
2014-05-20 15:40:07 +0200

    moment.js added to project for dates manipulation and display

---
+++ www/js/filters.js
@@ -0,0 +1,16 @@
+(function (angular, moment) {
+  'use strict';
+
+  angular.module('redion.filters', [])
+
+  .filter('moment', function () {
+    return function (input, format) {
+      if (!input || !format) {
+        return input;
+      } else {
+        return moment(input).format(format);
+      }
+    };
+  });
+
+}(angular, moment));
